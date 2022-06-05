99d2ddae69ff31f55f93560435b8ac8f03e6bc69
vkbbvcvnvvv <94930978+vkbbvcvnvvv@users.noreply.github.com>
2022-06-05 07:09:53 +0200

    Add files via upload

---
+++ Anonymous_VIB.py
@@ -0,0 +1,244 @@
+import os
+import time
+os.system ("clear")
+print ("\033[1;31m==============================================================")
+print ("\033[1;31m  _     _   _   _____")
+print ("\033[1;32m| |   / / | | |  _  /")
+print ("\033[1;31m| |  / /  | | | |_| |")
+print ("\033[1;32m| | / /   | | |  ___/")
+print ("\033[1;31m| |/ /    | | | |")
+print ("\033[1;32m|___/     |_| |_|")
+print ("\033[1;31maAnonymous_various \033[1;31m8x \033[1;31maattack")
+print ("\033[0;32mG mail\033[1;33m=============>\033[1;31mairc418@gmail.com")
+print ("\033[1;30mBlack had and \033[1;31manonymous")
+print ("\033[1;32mTelegram https://t.me/+SjiEVF8WrHdhOGVi")
+print ("=========================================================================")
+#_____________________________
+print ("\033[1;31m[1] \033[1;33install anonymous")
+name = input ("Chose Nuber : ")
+if name == "1" :
+#_________________
+    os.system ("clear")
+    print ("\033[1;31m[1] \033[1;33minstall root on termux")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[2] \033[1;33minstall metasploit")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[3] \033[1;33minstall ngrok")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[4] \033[1;33minstall hammer")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[5] \033[1;33minstall hack.MTR")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[6] \033[1;33minstall sqlmap")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[7] \033[1;33minstall VISA-CARD")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[8] \033[1;33minstall Machinae")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[9] \033[1;33minstall hunner")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[10] \033[1;33minstall CardPwn")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[11] \033[1;33minstall crips")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[12] \033[1;33minstall Cam-Hackers")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[13] \033[1;33minstall JokerTools")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[14] \033[1;33minstall PhoneInfoga")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[15] \033[1;33minstall fbi")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[16] \033[1;33minstall Dos Attack  ")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[17] \033[1;33minstall maltego in kali  ")
+    time.sleep(0.3)
+#__________________
+    print ("\033[1;31m[18] \033[1;33minstall Nmap")
+    time.sleep(0.3)
+#__________________
+name = input ("Chose Nuber : ")
+if name == "1" :
+     os.system ("apt-get update && apt-get upgrade -y")
+     os.system ("apt-get install wget -y")
+     os.system ("cd")
+     os.system ("git clone https://github.com/MFDGaming/ubuntu-in-termux.git")
+     os.system ("ls")
+     os.system ("cd ubuntu-in-termux")
+     os.system ("ls")
+     os.system ("chmod +x ubuntu.sh")
+     os.system ("./ubuntu.sh -y")
+     os.system ("./startubuntu.sh")
+#_________________
+if name == "2" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("pkg install wget -y")
+     os.system ("cd")
+     os.system ("wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh")
+     os.system ("chmod +x metasploit.sh")
+     os.system ("./metasploit.sh")
+     os.system ("msfconsole")
+#________________
+if name == "3" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("apt install git -y")
+     os.system ("cd")
+     os.system ("git clone https://github.com/Err0r-ICA/Ngrok.git")
+     os.system ("cd Ngrok")
+     os.system ("chmod +x termux-ngrok.sh")
+     os.system ("sh termux-ngrok.sh")
+#________________
+if name == "4" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("pkg install python -y")
+     os.system ("pkg install python2 -y")
+     os.system ("pkg install git -y")
+     os.system ("cd")
+     os.system ("git clone https://github.com/cyweb/hammer")
+     os.system ("ls")
+     os.system ("cd hammer")
+     os.system ("chmod +x hammer.py")
+     os.system ("python3 hammer.py")
+#________________
+if name == "5" :
+      os.system ("apt update && apt upgrade -y")
+      os.system ("apt insstall python -y")
+      os.system ("apt install python3 -y")
+      os.system ("apt install python2 -y")
+      os.system ("cd")
+      os.system ("git clone https://github.com/M-T-R/hack.MTR.git")
+      os.system ("cd hack.MTR")
+#________________
+if name == "6" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("apt install python -y")
+     os.system ("apt install python2 -y")
+     os.system ("apt install python3 -y")
+     os.system ("apt install git -y")
+     os.system ("cd")
+     os.system ("git clone https://github.com/sqlmapproject/sqlmap.git")
+     os.system ("cd sqlmap")
+     os.system ("python2 sqlmap.py")
+#________________
+if name == "7" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("apt install python -y")
+     os.system ("apt install python2 -y")
+     os.system ("pkg install php -y")
+     os.system ("apt install bash -y")
+     os.system ("cd")
+     os.system ("git clone https://github.com/Kasper-Dz/VISA-CARD.git")
+     os.system ("cd VISA-CARD")
+     os.system ("chmod 777 *")
+     os.system ("bash Card.sh")
+#________________
+if name == "8" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("apt install git -y")
+     os.system ("cd")
+     os.system ("git clone https://github.com/HurricaneLabs/machinae")
+#_______________
+if name == "9" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("apt install python python2 python3 -y")
+     os.system ("apt install git -y")
+     os.system ("cd")
+     os.system ("git clone https://github.com/b3-v3r/Hunner")
+     os.system ("cd Hunner")
+     os.system ("chmod 777 hunner.py")
+     os.system ("python hunner.py")
+#_______________
+if name == "10" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("apt install python3 -y")
+     os.system ("apt install git -y")
+     os.system("cd")
+     os.system ("git clone https://github.com/itsmehacker/CardPwn")
+     os.system ("cd CardPwn")
+     os.system ("chmod 777 *")
+     os.system ("pip3 install -r requirements.txt")
+     os.system ("python3 cardpwn.py")
+#_______________
+if name == "11" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("apt install git -y")
+     os.system ("pkg install python2 -y")
+     os.system ("cd")
+     os.system ("git clone https://github.com/Manisso/Crips")
+     os.system ("cd Crips")
+     os.system ("python2 crips.py")
+#_______________
+if name == "12" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("apt install git -y")
+     os.system ("cd")
+     os.system ("git clone https://github.com/AngelSecurityTeam/Cam-Hackers")
+     os.system ("pip3 install requests")
+     os.system ("cd Cam-Hackers")
+     os.system ("python3 cam-hackers.py")
+#_______________
+if name == "13" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("apt install git -y")
+     os.system ("apt install python pytho2 python3 -y")
+     os.system("cd")
+     os.system ("git clone https://github.com/RedVirus-Dev/JokerTools.git")
+     os.system ("cd JokerTools")
+     os.system ("python3 Jok3rTo0ls-V1.3.py")
+#_______________
+if name == "14" :
+     os.system ("apt update pkg upgrade -y")
+     os.system ("termux-setup-storage ")
+     os.system ("pkg install python  -y")
+     os.system ("pkg install git -y")
+     os.system ("cd")
+     os.system ("git clone https://github.com/abhinavkavuri/PhoneInfoga")
+     os.system ("cd PhoneInfoga")
+     os.system ("ls")
+     os.system ("mv config.example.py config.py")
+     os.system ("python3 -m pip install -r requirements.txt")
+     os.system ("chmod +x * ")
+     print ("If you want to occupy the tool python3 phoneinfoga.py -n +2010xxxxxxxx")
+#_______________
+if name == "15" :
+     os.system ("pkg install python2-dev python2 git -y")
+     os.system ("cd")
+     os.system ("git clone https://github.com/xHak9x/fbi.git")
+     os.system ("cd fbi")
+     os.system ("chmod 777  fbi.py")
+     os.system ("python2 fbi.py")
+#_______________
+if name == "16" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("apt install git -y")
+     os.system ("apt install python python2 python3 -y")
+     os.system ("cd")
+     os.system ("git clone https://github.com/Jmeel/Doosip.git")
+     os.system ("cd Doosip")
+     os.system ("python3 Doosip.py")
+#_______________
+if namme == "17" :
+      os.system ("nh -r kex")
+      os.system ("apt-get install maltego -y")
+#_______________
+if name == "18" :
+      os.system ("nh -r kex")
+      os.system ("apt-get install nmap -y")
+#_______________
+
